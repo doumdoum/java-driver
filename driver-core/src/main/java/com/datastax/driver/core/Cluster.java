ac61eeb900cbe4d5577ca7189fff693949027408
olim7t <omichallat+github@gmail.com>
2015-08-06 12:38:10 +0200

    Make connection init async (internally) (JAVA-821).

--- driver-core/src/main/java/com/datastax/driver/core/Cluster.java
+++ driver-core/src/main/java/com/datastax/driver/core/Cluster.java
@@ -1643,7 +1643,7 @@ public class Cluster implements Closeable {
                     // Now, check if there isn't pools to create/remove following the addition.
                     // We do that now only so that it's not called before we've set the node up.
                     for (SessionManager s : sessions)
-                        s.updateCreatedPools();
+                        s.updateCreatedPools().get();
 
                 } finally {
                     host.notificationsLock.unlock();
@@ -1912,7 +1912,7 @@ public class Cluster implements Closeable {
                     // Now, check if there isn't pools to create/remove following the addition.
                     // We do that now only so that it's not called before we've set the node up.
                     for (SessionManager s : sessions)
-                        s.updateCreatedPools();
+                        s.updateCreatedPools().get();
 
                 } finally {
                     host.notificationsLock.unlock();
@@ -2260,8 +2260,12 @@ public class Cluster implements Closeable {
             if (ccHost == null || loadBalancingPolicy().distance(ccHost) != HostDistance.LOCAL)
                 controlConnection.triggerReconnect();
 
-            for (SessionManager s : sessions)
-                s.updateCreatedPools();
+            try {
+                for (SessionManager s : sessions)
+                    Uninterruptibles.getUninterruptibly(s.updateCreatedPools());
+            } catch (ExecutionException e) {
+                throw Exceptions.toUnchecked(e);
+            }
         }
 
         void refreshConnectedHost(Host host) {
